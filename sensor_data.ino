ac107cdcd1516f01bab943bbeeb20c4f684d7720
formationBgojo <164862353+formationBgojo@users.noreply.github.com>
2025-07-04 22:50:53 +0400

    Create sensor_data.ino

---
+++ sensor_data.ino
@@ -0,0 +1,19 @@
+#include "Arduino_BMI270_BMM150.h"
+void setup() {
+Serial.begin(9600);
+while (!Serial);
+if (!IMU.begin()) {
+Serial.println("IMU initialization failed !");
+while(1);
+}
+}
+void loop() {
+float x,y,z;
+if (IMU.accelerationAvailable()) {
+IMU.readAcceleration(x,y,z);
+Serial.print("Acceleration : X="); Serial.print(x);
+Serial.print("Y="); Serial.print(y);
+Serial.print("Z="); Serial.println(z);
+}
+delay(500);
+}
